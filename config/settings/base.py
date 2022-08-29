e01a66815039eb87477c53847c7cb790bc78ea52
Do Thanh <thanhcoral@gmail.com>
2022-08-29 10:26:24 +0700

    up

--- config/settings/base.py
+++ config/settings/base.py
@@ -34,7 +34,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = '{{ project_name }}.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -52,7 +52,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
--- config/settings/dev.py
+++ config/settings/dev.py
@@ -3,7 +3,7 @@ from .base import *
 DEBUG = True
 
 THIRD_PARTY_APPS += [
-    'django_debug_toolbar',
+    # 'django_debug_toolbar',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
